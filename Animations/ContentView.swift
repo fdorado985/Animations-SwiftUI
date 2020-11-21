7ca657ff43c8307f748100701f577eb8c42e973b
Juan Dorado <fdorado985@gmail.com>
2020-11-20 23:44:31 -0600

    Initial Commit

---
+++ Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Juan Francisco Dorado Torres on 20/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
