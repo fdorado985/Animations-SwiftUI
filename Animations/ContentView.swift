b35729a5b6dd3d116c32783dcab3e74bee161fdf
Juan Dorado <fdorado985@gmail.com>
2020-11-22 00:31:28 -0600

    Create a card with a gradient

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,15 +11,15 @@ struct ContentView: View {
   @State private var enabled = false
 
   var body: some View {
-    Button("Tap Me") {
-      self.enabled.toggle()
-    }
-    .frame(width: 200, height: 200)
-    .background(enabled ? Color.blue : Color.red)
-    .animation(nil)
-    .foregroundColor(.white)
-    .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-    .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+    LinearGradient(
+      gradient: Gradient(colors: [.yellow, .red]),
+      startPoint: .topLeading,
+      endPoint: .bottomTrailing
+    )
+    .frame(width: 300, height: 200)
+    .clipShape(
+      RoundedRectangle(cornerRadius: 10)
+    )
   }
 }
 
