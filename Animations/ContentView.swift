5d2a2610c3d3b032360d4cd18d7f128e8a632620
Juan Dorado <fdorado985@gmail.com>
2020-11-22 01:13:04 -0600

    Build a custom transition using a ViewModifier

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -28,6 +28,26 @@ struct ContentView: View {
   }
 }
 
+struct CornerRotateModifier: ViewModifier {
+  let amount: Double
+  let anchor: UnitPoint
+
+  func body(content: Content) -> some View {
+    content
+      .rotationEffect(
+        .degrees(amount),
+        anchor: anchor
+      )
+      .clipped()
+  }
+}
+
+extension AnyTransition {
+  static var pivot: AnyTransition {
+    .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+  }
+}
+
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
